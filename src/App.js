f4f30d5702aef61094742b9fb81789a0f855fab8
James Medd <james.medd@sparck.io>
2023-12-19 11:25:10 +0000

    Encrypt text as it passes in and out of localStorage

--- src/App.js
+++ src/App.js
@@ -11,19 +11,30 @@ import { TimeRemaining } from './components/TimeRemaining.js'
 import { PageHeading } from './components/Typography.js'
 import { AboutIcon } from './components/icons/AboutIcon.js'
 import { AboutModal } from './components/AboutModal.js'
+import { SimpleCrypto } from './SimpleCrypto.js'
 
 function App () {
+  const sc = new SimpleCrypto()
   const { isDarkMode, toggle } = useDarkMode()
-  const [message, setMessage] = useLocalStorage('message', window.localStorage.getItem('message') ?? '')
+  const [writing, setWriting] = useLocalStorage('writing', window.localStorage.getItem('writing') ?? '')
+  const [displayedWriting, setDisplayedWriting] = useState('')
   const [timeRemaining, setTimeRemaining] = useState(0)
   const [displayingAbout, setDisplayingAbout] = useState(false)
 
+  useEffect(() => {
+    if (writing.length > 0) sc.decryptText(writing).then(text => setDisplayedWriting(text))
+    console.log('decrypted')
+  }, [writing])
+
   const handleChange = event => {
-    setMessage(event.target.value.replace(/[^\S\n][^\S\n]+/g, ' ').replace(/\n[^\S\n]/, '\n'))
+    const incomingWriting = event.target.value.replace(/[^\S\n][^\S\n]+/g, ' ').replace(/\n[^\S\n]/, '\n')
+    setDisplayedWriting(incomingWriting)
+    sc.encryptText(incomingWriting).then(text => setWriting(text))
   }
   useEffect(() => {
     if (!window.localStorage.getItem('expiryDate') || new Date() > new Date(window.localStorage.getItem('expiryDate'))) {
-      setMessage('')
+      setWriting('')
+      setDisplayedWriting('')
       const expiryDate = new Date()
       expiryDate.setHours(23, 59, 59)
       window.localStorage.setItem('expiryDate', expiryDate)
@@ -50,10 +61,10 @@ function App () {
           </ButtonToolbar>
         </StyledSection>
         <StyledSection grow>
-          <FadingTextInput value={message} onChange={handleChange} accessibleLabel='Text you want to disappear' placeholder='Write here...' />
+          <FadingTextInput id='text-do-disappear' value={displayedWriting} onChange={handleChange} accessibleLabel='Text you want to disappear' placeholder='Write here...' />
         </StyledSection>
         <StyledSection>
-          <TimeRemaining seconds={timeRemaining} visible={message.length > 0} />
+          <TimeRemaining seconds={timeRemaining} visible={displayedWriting.length > 0} />
         </StyledSection>
       </StyledMain>
     </ThemeProvider>
