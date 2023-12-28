b81b072d41a4f68e770007e8e41613f85e5c2d98
James Medd <james.medd@sparck.io>
2023-12-28 15:18:09 +0000

    Get ready for a PWA

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,6 +18,47 @@ export default defineConfig({
             }
           ]
         ]
+      }
+    }),
+    VitePWA({
+      workbox: {
+        globPatterns: ['**/*']
+      },
+      includeAssets: [
+        '**/*'
+      ],
+      manifest: {
+        name: 'Delible ink.',
+        short_name: 'Delible ink.',
+        icons: [
+          {
+            src: '/android-chrome-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: '/android-chrome-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: '/maskable-icon.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ],
+        theme_color: '#faf5e9',
+        background_color: '#faf5e9',
+        start_url: 'https://delible.ink',
+        display: 'standalone',
+        orientation: 'portrait'
       }
     })
   ]
