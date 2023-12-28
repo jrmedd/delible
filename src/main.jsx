b8205f0f42c8b12a3dba7da826b7446fc000c10a
James Medd <james.medd@sparck.io>
2023-12-28 15:19:18 +0000

    Get ready for a PWA (#17)

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+  onNeedRefresh () {
+    if (window.confirm('New content available. Reload?')) {
+      updateSW(true)
+    }
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
